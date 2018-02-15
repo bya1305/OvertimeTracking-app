60804ad170fe172241e13bc92e4664aff210b002
Optimus <github email address>
2018-02-15 10:11:52 -0800

    added status field to admin dashboard views, added radio buttons for post status on forms

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 @seeduser = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
-
+AdminUser.create(email: "admin@admin.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Will", last_name: "Turner")
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @seeduser.id)
 end
