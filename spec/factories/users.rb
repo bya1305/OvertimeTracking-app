1ed0f58f90ec70fcad259f3f3529d97f541dc57d
Optimus <github email address>
2018-02-14 23:31:28 -0800

    added authorization check to assure that only admin users can access admin dashboard

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -11,7 +11,7 @@ FactoryBot.define do
     password_confirmation "asdfasdf"
   end
 
-  factory :admin, class: "User" do
+  factory :admin_user, class: "AdminUser" do
     first_name "Admin"
     last_name "User"
     email {generate :email}
